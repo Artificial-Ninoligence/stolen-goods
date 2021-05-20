32c2537f739eb23b3b77a137ae71e55e79f62531
NlNOVATlON <ninovationlab@gmail.com>
2021-05-20 10:09:44 +0200

    Selenium test skipped for now

---
+++ accounts/tests/unit/test_selenium.py
@@ -0,0 +1,39 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+from time import sleep
+import pytest
+
+@pytest.mark.skip(reason="No CI setup for selenium in Github Actions")
+class TestLoginForm(LiveServerTestCase):
+
+    def setUp(self):
+
+        self.cm_driver = webdriver.Chrome('drivers/chromedriver')
+
+    def test_login_form(self):
+
+        cm_driver = self.cm_driver
+
+        cm_driver.get('http://127.0.0.1:8000/accounts/login/')
+        cm_driver.find_element_by_id('login-email').send_keys('nino.lindenberg@code.berlin')
+        cm_driver.find_element_by_id('login-password').send_keys('password123!')
+        cm_driver.find_element_by_id('login-submit').click()
+        sleep(3)
+
+        assert 'You are now logged in.' in cm_driver.page_source
+
+    def test_admin_login_form(self):
+
+        cm_driver = self.cm_driver
+
+        cm_driver.get('http://127.0.0.1:8000/stolen-goods-admin/')
+        cm_driver.find_element_by_id('id_username').send_keys('admin@sg.wtf')
+        cm_driver.find_element_by_id('id_password').send_keys('admin')
+        cm_driver.find_element_by_css_selector('input[type=submit]').click()
+        sleep(3)
+
+        assert 'Django administration' in cm_driver.page_source
+
+    def tearDown(self):
+        self.cm_driver.quit()
--- accounts/tests/unit/test_views.py
+++ accounts/tests/unit/test_views.py
@@ -1,9 +1,6 @@
-from django.test import TestCase, LiveServerTestCase, Client
+from django.test import TestCase, Client
 # from django.urls import reverse
-from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-from time import sleep
-from accounts.views import register, login
+from accounts.views import register, login, logout
 # import pytest
 
 
@@ -21,36 +18,11 @@ class TestRegisterView(TestCase):
         self.assertEqual(response.resolver_match.func, register)
 
 
-class TestLoginView(LiveServerTestCase):
+class TestLoginView(TestCase):
 
     def setUp(self):
 
-        self.cm_driver = webdriver.Chrome('drivers/chromedriver')
         self.client = Client()
-
-    def test_login_form(self):
-
-        cm_driver = self.cm_driver
-
-        cm_driver.get('http://127.0.0.1:8000/accounts/login/')
-        cm_driver.find_element_by_id('login-email').send_keys('nino.lindenberg@code.berlin')
-        cm_driver.find_element_by_id('login-password').send_keys('password123!')
-        cm_driver.find_element_by_id('login-submit').click()
-        sleep(3)
-
-        assert 'You are now logged in.' in cm_driver.page_source
-
-    def test_admin_login_form(self):
-
-        cm_driver = self.cm_driver
-
-        cm_driver.get('http://127.0.0.1:8000/stolen-goods-admin/')
-        cm_driver.find_element_by_id('id_username').send_keys('admin@sg.wtf')
-        cm_driver.find_element_by_id('id_password').send_keys('admin')
-        cm_driver.find_element_by_css_selector('input[type=submit]').click()
-        sleep(3)
-
-        assert 'Django administration' in cm_driver.page_source
 
     def test_login_template_and_url_status_code_200(self):
 
@@ -59,5 +31,17 @@ class TestLoginView(LiveServerTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.func, login)
 
-    def tearDown(self):
-        self.cm_driver.quit()
+class TestLogoutView(TestCase):
+
+    def setUp(self):
+
+        self.client = Client()
+
+    def test_logout_redirect_and_url_status_code_200(self):
+
+        response = self.client.get('/accounts/logout/')
+        expected_url = '/accounts/login/?next=%2Faccounts%2Flogout%2F'
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.resolver_match.func, logout)
+        self.assertRedirects(response, expected_url, status_code=302, target_status_code=200)
